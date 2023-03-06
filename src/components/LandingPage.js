d2da896a288eb68aa63490651ae275e257f1608e
david <davmwa90@gmail.com>
2023-03-06 07:11:20 +0300

    updated the css

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,12 +1,15 @@
 import React from "react";
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
 
 function LandingPage() {
   return (
     <div className="landing">
-      <h1>Welcome to the Project Management System</h1>
-        <Link to="/projects">View Projects</Link>
-        <Link to="/create-project">Create a New Project</Link>
+      <h1>Welcome to Project Management Application</h1>
+      <label for="username">Username</label>
+        <input type="text" placeholder = "Enter Username" name="username"/>
+      <label for="password">Password</label>
+        <input type="text" placeholder = "Enter Password" name="password"/>
+      <button id = "button" type="submit">Login</button>
     </div>
   );
 }
